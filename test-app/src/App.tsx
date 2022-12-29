b02b906e037eaffd49d9f2f44efe2692e97b1610
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2022-12-29 18:55:50 +0100

    test commit

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          THIS IS TEST!
         </a>
       </header>
     </div>
