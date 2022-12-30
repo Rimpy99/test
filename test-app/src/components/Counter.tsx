12ae81839421141a79b168bb7a665635f4c63120
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2022-12-30 12:25:47 +0100

    counter created

--- test-app/src/components/Counter.tsx
+++ test-app/src/components/Counter.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useCounterStore } from '../stores/counterStore/counterStore';
 
 export const Counter = () => {
 
+    const counter = useCounterStore(state => state.count);
+    const increaseCounter = useCounterStore(state => state.increaseCounter);
+    const decreaseCounter = useCounterStore(state => state.decreaseCounter);
+
     return (
         <>
-            
+            {counter}
+            <button onClick={increaseCounter}>INCREASE</button>
+            <button onClick={decreaseCounter}>DECREASE</button>
         </>
     )
 }
--- test-app/src/stores/counterStore/counterStore.ts
+++ test-app/src/stores/counterStore/counterStore.ts
@@ -2,9 +2,11 @@ import create from 'zustand';
 
 type CounterType = {
     count: number,
+    increaseCounter: () => void,
+    decreaseCounter: () => void,
 }
 
-const useCounterStore = create<CounterType>((set) => ({
+export const useCounterStore = create<CounterType>((set) => ({
     count: 0,
     increaseCounter: () => set(state => ({ count: state.count + 1 })),
     decreaseCounter: () => set(state => ({ count: state.count - 1 })),
