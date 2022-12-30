30b21377bc6638f0147e9a437da09ddf7f42d857
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2022-12-30 16:05:23 +0100

    styled components installed

--- test-app/src/components/ColorChanger.tsx
+++ test-app/src/components/ColorChanger.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useColorStore } from '../stores/colorStore/colorStore';
 
 export const ColorChanger = () => {
     
+    const color = useColorStore(state => state.color);
+    const changeColor = useColorStore(state => state.changeColor);
+
     return (
-        
+        <>
+            <button onClick={() => changeColor('green')}>GREEN</button>
+            <button onClick={() => changeColor('orange')}>ORANGE</button>
+            <button onClick={() => changeColor('pink')}>PINK</button>
+        </>
     )
 }
